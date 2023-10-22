d30f6a73bfa8a27b4c954521b0dee3fb7960f8eb
Minh Tâm <102682115+minhtam78945@users.noreply.github.com>
2023-10-22 13:10:27 +0700

    Initial commit

---
+++ coureseapp/coures/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Category,Course
+class CategoryAmin(admin.ModelAdmin):
+    list_display = ['id','name']
+# Register your models here.
+admin.site.register(Category,CategoryAmin)
+admin.site.register(Course)
